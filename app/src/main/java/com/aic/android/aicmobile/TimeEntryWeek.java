ddaf937264450ebb5eaa2763632f18ec77090a26
jmaddy89 <jmaddy89@gmail.com>
2017-04-12 17:05:02 -0400

    Added to TimeEntryWeek class

--- app/src/main/java/com/aic/android/aicmobile/TimeEntryWeek.java
+++ app/src/main/java/com/aic/android/aicmobile/TimeEntryWeek.java
@@ -1,6 +1,8 @@
 package com.aic.android.aicmobile;
 
-import java.util.Date;
+import com.aic.android.aicmobile.backend.aicDataAPI.model.TimeEntryDay;
+
+import java.util.List;
 
 /**
  * Created by JLM on 4/11/2017.
@@ -10,6 +12,7 @@ public class TimeEntryWeek {
     private String dayName;
     private String date;
     private float hours;
+    private List<TimeEntryDay> timeEntryDayList;
 
     public String getDayName() {
         return dayName;
@@ -23,9 +26,14 @@ public class TimeEntryWeek {
         return hours;
     }
 
+    public List<TimeEntryDay> getTimeEntryDayList() {
+        return timeEntryDayList;
+    }
+
     public void setDayName(String dayName) {
         this.dayName = dayName;
     }
+
 
     public void setDate(String date) {
         this.date = date;
@@ -33,5 +41,9 @@ public class TimeEntryWeek {
 
     public void setHours(float hours) {
         this.hours = hours;
+    }
+
+    public void setTimeEntryDayList(List<TimeEntryDay> timeEntryDayList) {
+        this.timeEntryDayList = timeEntryDayList;
     }
 }
